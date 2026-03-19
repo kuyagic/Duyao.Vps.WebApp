de605fc597c012ee2db6255118eea2bfa33b5305
Chinni <kuyagic@gmail.com>
2026-03-19 08:56:46 +0800

    添加Aot 定义

--- Duyao.NsTunnel/AppConfig.cs
+++ Duyao.NsTunnel/AppConfig.cs
@@ -1,5 +1,15 @@
-﻿namespace Duyao.NsTunnel;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
+namespace Duyao.NsTunnel;
+
+// 假设您已经有一个名为 AppJsonSerializerContext 的上下文
+[JsonSourceGenerationOptions(WriteIndented = false)] // 根据需要配置
+[JsonSerializable(typeof(AppConfig))]
+[JsonSerializable(typeof(JsonElement))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+}
 public class AppConfig
 {
     
